8b5c04e1d593183bacdcbf67d417a990774b9cec
eirafle <aleksandrs.fridmans@ericsson.com>
2021-09-10 15:01:18 +0100

    initial commit

---
+++ src/main/java/com/alexnail/frtechassignment/FRTechAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.alexnail.frtechassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FRTechAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FRTechAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexnail/frtechassignment/FRTechAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexnail.frtechassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FRTechAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
